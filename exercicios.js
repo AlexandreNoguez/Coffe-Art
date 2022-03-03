5e4e6222cb8ada1804d06da69c0e4a3942178d85
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2022-03-03 03:54:14 -0300

    adicionado exercicios JS

---
+++ exercicios.js
@@ -0,0 +1,168 @@
+//exercicio1==================
+// Declare uma variável chamada 'nome'
+// Atribua um nome como valor da variável
+// Descomente a última linha
+
+//let nome = "Alex";
+
+// alert('Olá, seja bem-vindo(a), ' + nome + '!')
+
+// if (nome !== undefined) {
+//   const mensagem = document.getElementById('mensagem');
+//   mensagem.classList.remove('hidden');
+// }
+
+// CSS========================
+
+// p {
+//     padding: 12px;
+//     background: #CFC;
+//     font-size: 16px;
+//     font-weight: bold;
+//     font-family: sans-serif;
+//   }
+  
+//   .hidden {
+//     display: none;
+//   }
+
+// HTML
+
+// <p class="hidden" id="mensagem">Muito bem! Você acertou!</p>
+
+// exercicio 2
+
+// Hora de incrementar um pouco!
+// Declare, uma variável chamada 'primeiroNome'
+// E também uma variável chamada 'ultimoNome'
+// Atribua o seu primeiro e último nome às respectivas variáveis
+// Descomente a linha 10
+
+// let ____________ = "_________";
+// let __________ = "_________";
+
+// alert('Meu nome completo é: ' + primeiroNome + ' ' + ultimoNome);
+
+// if (primeiroNome !== undefined && ultimoNome !== undefined) {
+//   const mensagemTag = document.getElementById('mensagem');
+//   const mensagem = "Muito bem, " + primeiroNome + " " + ultimoNome + "! Você acertou!";
+//   mensagemTag.innerText = mensagem;
+//   mensagemTag.classList.remove('hidden');
+// }
+
+// exercicio 3
+
+// Complete o campo vázio e descomente a última linha para ver o total na tela
+
+// *Obs: para descomentar um código você pode:
+// apertar ctrl + / ou
+// simplesmente apagar as duas barras no início da linha
+
+// Preço 1 = 4
+// Preço 2 = 7
+// Total = 4 + 7 = 11
+
+// let preco1 = 4;
+// let preco2 = 7;
+// let total = preco1 + ______;
+
+// alert('Total: '+total);
+
+// if (total !== undefined) {
+//   const mensagem = document.getElementById('mensagem');
+//   mensagem.classList.remove('hidden');
+// }
+
+// exercicio 4
+
+// Crie três variaveis: 'numero1', 'numero2' e 'soma'
+// Atribua numero1 + numero2 para a variável 'soma'
+// Descomente a última linha
+
+// let _______ = 9;
+// let _______ = 7;
+// let ____ = _______ + numero2;
+
+// alert('A soma de  ' + numero1 + ' com ' + numero2 + ' é ' + soma);
+
+// if (numero1 !== undefined && numero2 !== undefined && soma !== undefined) {
+//   const mensagem = document.getElementById('mensagem');
+//   mensagem.classList.remove('hidden');
+// }
+
+// exercicio 5
+// Substitua no underline(_) entre as variaveis para que a condição
+// "numero1 é maior que número2"
+// retorne true.
+
+// let numero1 = 10;
+// let numero2 = 5;
+
+// let resultado = numero1 _ numero2;
+
+// alert(resultado);
+
+// if(resultado !== undefined && resultado === true){
+//   const mensagem = document.getElementById('mensagem');
+//   mensagem.classList.remove('hidden');
+// }
+ 
+// exercicio 6
+
+// Substitua no underline(_) entre as variaveis para que a condição
+// "numero1 é igual ao número2"
+// retorne true.
+
+// let numero1 = 7;
+// let numero2 = 7;
+
+// let resultado = numero1 == numero2;
+
+// alert(resultado);
+
+// if(resultado !== undefined && resultado === true){
+//   const mensagem = document.getElementById('mensagem');
+//   mensagem.classList.remove('hidden');
+// }
+
+// exercicio 7
+
+// Escreva um código que imprime um 'Bom dia!'
+// no console se a hora for menor que 12
+
+// let horaManha = 11;
+
+// if(horaManha _ 12){
+  // executa se a condição for verdadeira
+  // console.log('');
+// }
+
+// Utilize o else, um bloco que será executado
+// caso a condição seja false
+// Faça com que imprime um 'Boa tarde!' caso
+// a condição seja false
+
+// let horaTarde = 15;
+
+// if(horaTarde _ 12){
+  // executa se a condição for verdadeira
+  // console.log('');
+// } else{
+  // executa se a condição for falsa
+  // console.log('');
+// }
+
+// exercicio 8
+
+// Escreva um código que imprime o maior número entre dois números
+
+// let numero1 = 7;
+// let numero2 = 9;
+
+// if(numero1 _ numero2){
+  // executa se a condição for verdadeira
+  // console.log(numero_);
+// } else{
+  // executa se a condição for falsa
+  // console.log(numero_);
+// }
